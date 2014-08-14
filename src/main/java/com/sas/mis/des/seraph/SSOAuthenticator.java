9cc730f95f49a6419237208cc809d95e9894162b
dashap <david.shapiro@sas.com>
2014-08-14 12:37:32 -0400

    Update SSOAuthenticator.java

--- src/main/java/com/sas/mis/des/seraph/SSOAuthenticator.java
+++ src/main/java/com/sas/mis/des/seraph/SSOAuthenticator.java
@@ -7,49 +7,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.security.Principal;
 
-/**
- * Extension of DefaultAuthenticator that uses third-party code to determine if a user is logged in,
- * given a HTTPRequest object.
- * Third-party code will typically check for the existence of a special cookie.
- *
- * In SSO scenarios where this authenticator is used, one typically configures Seraph to use an external login page
- * as well:
- *  *  <init-param>
- *    <param-name>login.url</param-name>
- *    <param-value>http://mycompany.com/globallogin?target=${originalurl}</param-value>
- *  </init-param>
- *
- *
- */
 public class SSOAuthenticator extends JiraSeraphAuthenticator
 {
-
-    //private static final Category log = Category.getInstance(SSOAuthenticator.class);
     private static final Logger log = Logger.getLogger(SSOAuthenticator.class);
-
-    /*
-    @Override
-    protected Principal getUser(final String s) {
-        return new Principal() {
-            @Override
-            public String getName() {
-                return s;
-            }
-        };
-    }
-
-    @Override
-    protected boolean authenticate(Principal principal, String s) throws AuthenticatorException {
-        return false;
-    }
-    */
 
     public Principal getUser(HttpServletRequest request, HttpServletResponse response)
     {
         Principal user = null;
-
-        //Principal user = request.getUserPrincipal();
-        // userName = userPrincipal.getName();
 
         try
         {
@@ -59,45 +23,22 @@ public class SSOAuthenticator extends JiraSeraphAuthenticator
                 String username = user.getName();
                 log.info("Session found; user " + username + " is already logged in");
             }
-            else {
+            else 
+            {
                 user = request.getUserPrincipal();
                 String username = user.getName();
 
-                if (null != username) {
-                    log.info("Logged in via getUserPrincipal, with User " + username);
+                if (null != username) 
+                {
+                    log.info("Logged in using SSO via getUserPrincipal, with User " + username);
                     request.getSession().setAttribute(JiraSeraphAuthenticator.LOGGED_IN_KEY, user);
                     request.getSession().setAttribute(JiraSeraphAuthenticator.LOGGED_OUT_KEY, null);
                 }
                 else
                 {
-                    if (null != request.getHeader("uid")) {
-                        username = request.getHeader("uid");
-                        log.info("Logged in via getHeader, with User " + username);
-                        request.getSession().setAttribute(JiraSeraphAuthenticator.LOGGED_IN_KEY, user);
-                        request.getSession().setAttribute(JiraSeraphAuthenticator.LOGGED_OUT_KEY, null);
-                    } else {
-                        user = new Principal() {
-                            @Override
-                            public String getName() {
-
-                                return "dashap";
-                            }
-                        };
-                        log.info("Logged in via dashap as last resort");
-                        request.getSession().setAttribute(JiraSeraphAuthenticator.LOGGED_IN_KEY, user);
-                        request.getSession().setAttribute(JiraSeraphAuthenticator.LOGGED_OUT_KEY, null);
-                    }
+                    log.info("Did not login using SSO because user unknown");
                 }
             }
-                /*
-                else
-                {
-                    //log.info("SSOCookie is null; redirecting");
-                    log.info("Remote user is null; redirecting");
-                    //user was not found, or not currently valid
-                    return null;
-                }
-                */
         }
         catch (Exception e) // catch class cast exceptions
         {
